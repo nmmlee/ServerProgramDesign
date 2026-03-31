ef1d6f83b4fc81d11f75852a4ba953057045d666
Minyong Ha <pol5412@naver.com>
2026-03-31 11:47:17 +0900

    [Modify] Lovable 아이콘 제거, title 이름 수정

--- FE/src/main.tsx
+++ FE/src/main.tsx
@@ -2,4 +2,15 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+import cookingHero from "./assets/cooking-hero.png";
+
+let faviconLink = document.querySelector<HTMLLinkElement>("link[rel='icon']");
+if (!faviconLink) {
+  faviconLink = document.createElement("link");
+  faviconLink.rel = "icon";
+  document.head.appendChild(faviconLink);
+}
+faviconLink.type = "image/png";
+faviconLink.href = cookingHero;
+
 createRoot(document.getElementById("root")!).render(<App />);
